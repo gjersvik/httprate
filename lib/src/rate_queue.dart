2c75a4441223cd2827fe2261a19c7d26be38e7b4
Ole Martin Gjersvik <olem@olem.org>
2015-08-09 20:47:36 +0200

    Now runJob passes all tests and do something useful.

--- lib/src/rate_queue.dart
+++ lib/src/rate_queue.dart
@@ -1,16 +1,50 @@
 library httprate.rate_queue;
 
 import 'dart:async';
+import 'dart:collection';
 
 class RateQueue{
   int domainLimit = 6;
 
   RateQueue.independent();
 
-  Future runJob(String domain, Future job()) async{
+  Map<String,Queue> _queue = {};
+  Map<String,int> _running = {};
+
+  Future runJob(String domain, Future task()) async{
     if(domain == null){
       throw new ArgumentError.notNull('domain');
     }
-    return await job();
+    _queue.putIfAbsent(domain, ()=> new Queue());
+    _running.putIfAbsent(domain, ()=>0);
+    var job = new _Job(domain,task);
+    if(_canRun(job)){
+      _run(job);
+    }else{
+      _queue[domain].add(job);
+    }
+    return job.com.future;
   }
+
+  bool _canRun(_Job job) => _running[job.domain] < domainLimit;
+
+  _whenComplete(_Job job){
+    _running[job.domain] -= 1;
+    if(_queue[job.domain].isNotEmpty){
+      _run(_queue[job.domain].removeFirst());
+    }
+  }
+
+  _run(_Job job){
+    _running[job.domain] += 1;
+    var task = new Future(job.callback).whenComplete(() => _whenComplete(job));
+    job.com.complete(task);
+  }
+}
+
+class _Job{
+  final Completer com =  new Completer.sync();
+  final Function callback;
+  final domain;
+  _Job(this.domain, this.callback);
 }
